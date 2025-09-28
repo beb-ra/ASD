4c990e8c6bcce5a4376875a716fffee0cbcd1b03
bebra <gusol2468@gmail.com>
2025-09-28 18:25:19 +0300

    added math vector

--- lib_mvector/mvector.h
+++ lib_mvector/mvector.h
@@ -1,25 +1,59 @@
 //#include "../lib_tvector/tvector.h"
 #include "C://Users/Lelya/cc++/CMake/ASD/lib_tvector/tvector.h"
 
+template <class T> class MVector;
+template <class T>
+MVector<T> operator+(const MVector<T>&, const MVector<T>&);
+template <class T>
+MVector<T> operator-(const MVector<T>&, const MVector<T>&);
+template <class T>
+T operator*(const MVector<T>&, const MVector<T>&);
+template <class T>
+MVector<T> operator*(const MVector<T>&, const T);
+template <class T>
+MVector<T> operator*(const T, const MVector<T>&);
+
 template <class T>
 class MVector : private TVector<T> {
+protected:
 	size_t _start_index;
 
 public:
+	explicit MVector(size_t size = 0);
+	MVector(size_t start_index, size_t size);
 	MVector(const size_t size, const T* data, const size_t start_index);
 	MVector(const size_t size, const std::initializer_list<T> data, const size_t start_index);
 	MVector(const MVector<T>& other);
+
+	const size_t start_index() const noexcept;
+	void set_start_index(const size_t);
+
+	const size_t size() const noexcept;
+	const size_t capacity() const noexcept;
+
+	bool operator == (const MVector<T>& other) const noexcept;
+	bool operator != (const MVector<T>& other) const noexcept;
+
+	MVector<T>& operator=(const MVector<T>& other);
+	T& operator[](const int);
+	const T& operator[](const int) const;
 
 	MVector<T>& operator+=(const MVector<T>& other);
 	MVector<T>& operator-=(const MVector<T>& other);
 	MVector<T>& operator*=(const T);
 
-	friend MVector<T> operator+(const MVector<T>&, const MVector<T>&);
-	friend MVector<T> operator-(const MVector<T>&, const MVector<T>&);
-	friend T operator*(const MVector<T>&, const MVector<T>&);
-	friend MVector<T> operator*(const MVector<T>&, const T);
-	friend MVector<T> operator*(const T, const MVector<T>&);
+	friend MVector<T> operator+ <T>(const MVector<T>&, const MVector<T>&);
+	friend MVector<T> operator- <T>(const MVector<T>&, const MVector<T>&);
+	friend T operator* <T>(const MVector<T>&, const MVector<T>&);
+	friend MVector<T> operator* <T>(const MVector<T>& other, const T value);
+	friend MVector<T> operator* <T>(const T, const MVector<T>&);
 };
+
+template <class T>
+MVector<T>::MVector(size_t size) : TVector<T>(size), _start_index(0) {}
+
+template <class T>
+MVector<T>::MVector(size_t size, size_t start_index) : TVector<T>(size), _start_index(start_index) {}
 
 template <class T>
 MVector<T>::MVector(const size_t size, const T* data, const size_t start_index) : TVector<T>(size, data), _start_index(start_index) {}
@@ -27,3 +61,161 @@ MVector<T>::MVector(const size_t size, const T* data, const size_t start_index) 
 template <class T>
 MVector<T>::MVector(const size_t size, const std::initializer_list<T> data, const size_t start_index) : TVector<T>(size, data), _start_index(start_index) {}
 
+template <class T>
+MVector<T>::MVector(const MVector<T>& other) : TVector<T>(other), _start_index(other._start_index) {}
+
+template <class T>
+const size_t MVector<T>::size() const noexcept {
+	return TVector<T>::size();
+}
+template <class T>
+const size_t MVector<T>::capacity() const noexcept {
+	return TVector<T>::capacity();
+}
+
+template <class T>
+bool MVector<T>::operator == (const MVector<T>& other) const noexcept {
+	if (!static_cast<const TVector<T>&>(*this).operator==(static_cast<const TVector<T>&>(other))) {
+		return false;
+	}
+	
+	return _start_index == other._start_index;
+}
+
+template <class T>
+bool MVector<T>::operator != (const MVector<T>& other) const noexcept {
+	if (!static_cast<const TVector<T>&>(*this).operator!=(static_cast<const TVector<T>&>(other))) {
+		return false;
+	}
+
+	return _start_index != other._start_index;
+}
+
+template <class T>
+MVector<T>& MVector<T>::operator=(const MVector<T>& other) {
+	if (this == &other) {
+		return *this;
+	}
+	(*this).TVector<T>::operator=(other);
+	_start_index = other._start_index;
+}
+
+template <class T>
+T& MVector<T>::operator[](const int index) {
+	return (*this).TVector<T>::operator[](index);
+}
+
+template <class T>
+const T& MVector<T>::operator[](const int index) const {
+	return (*this).TVector<T>::operator[](index);
+}
+
+template <class T>
+MVector<T>& MVector<T>::operator+=(const MVector<T>& other) {
+	if (is_empty() || other.is_empty())
+		throw std::invalid_argument("The math vector is empty");
+
+	if (size() != other.size())
+		throw std::invalid_argument("Operations on vectors of different sizes aren't available");
+
+	for (int i = 0; i < size(); i++) {
+		(*this)[i] += other[i];
+	}
+	return *this;
+}
+
+template <class T>
+MVector<T>& MVector<T>::operator-=(const MVector<T>& other) {
+	if (is_empty() || other.is_empty())
+		throw std::invalid_argument("The math vector is empty");
+
+	if (size() != other.size())
+		throw std::invalid_argument("Operations on vectors of different sizes aren't available");
+
+	for (int i = 0; i < size(); i++) {
+		(*this)[i] -= other[i];
+	}
+	return *this;
+}
+
+template <class T>
+MVector<T>& MVector<T>::operator*=(const T value) {
+	if (is_empty())
+		throw std::invalid_argument("The math vector is empty");
+
+	for (int i = 0; i < size(); i++) {
+		(*this)[i] *= value;
+	}
+	return *this;
+}
+
+template <class T>
+MVector<T> operator*(const MVector<T>& other, const T value) {
+	if (other.is_empty())
+		throw std::invalid_argument("The math vector is empty");
+
+	MVector<T> result(other);
+	result *= value;
+	return result;
+}
+
+template <class T>
+MVector<T> operator*(const T value, const MVector<T>& other) {
+	if (other.is_empty())
+		throw std::invalid_argument("The math vector is empty");
+
+	MVector<T> result(other);
+	result *= value;
+	return result;
+}
+
+template <class T>
+T operator*(const MVector<T>& first, const MVector<T>& second) {
+	if (first.is_empty() || second.is_empty())
+		throw std::invalid_argument("The math vector is empty");
+
+	if (first.size() != second.size())
+		throw std::invalid_argument("Operations on vectors of different sizes aren't available");
+
+	T result = T();
+	for (int i = 0; i < first.size(); i++) {
+		result += first[i] * second[i];
+	}
+	return result;
+}
+
+template <class T>
+MVector<T> operator+(const MVector<T>& first, const MVector<T>& second) {
+	if (first.is_empty() || second.is_empty())
+		throw std::invalid_argument("The math vector is empty");
+
+	if (first.size() != second.size())
+		throw std::invalid_argument("Operations on vectors of different sizes aren't available");
+
+	MVector<T> result(first);
+	result += second;
+	return result;
+}
+
+template <class T>
+MVector<T> operator-(const MVector<T>& first, const MVector<T>& second) {
+	if (first.is_empty() || second.is_empty())
+		throw std::invalid_argument("The math vector is empty");
+
+	if (first.size() != second.size())
+		throw std::invalid_argument("Operations on vectors of different sizes aren't available");
+
+	MVector<T> result(first);
+	result -= second;
+	return result;
+}
+
+template <class T>
+const size_t MVector<T>::start_index() const noexcept {
+	return _start_index;
+}
+
+template <class T>
+void MVector<T>::set_start_index(const size_t index) {
+	_start_index = index;
+}
--- tests/test_mvector.cpp
+++ tests/test_mvector.cpp
@@ -1,39 +1,315 @@
 #include <gtest/gtest.h>
 #include "../lib_mvector/mvector.h"
 
-TEST(TestMVectorLib, try_create_object) {
-    bool expected_result = true;
-    bool actual_result = true;
 
+TEST(TestMVectorLib, try_create_object) {
+    ASSERT_NO_THROW(MVector<int> object);
+}
+
+TEST(TestMVectorLib, try_create_object_2) {
+    ASSERT_NO_THROW(MVector<int> object(10, 2));
+}
+
+TEST(TestMVectorLib, try_create_object3) {
     int size = 2;
     int* mass = new int[size];
     mass[0] = 1; mass[1] = 2;
     size_t index = 0;
 
-    try {
-        MVector<int> object(size, mass, index);
-    }
-    catch (const std::exception& ex) {
-        std::cerr << ex.what();
-        actual_result = false;
-    }
-
-    EXPECT_EQ(expected_result, actual_result);
+    ASSERT_NO_THROW(MVector<int> object(size, mass, index));
 }
 
-TEST(TestMVectorLib, try_create_object_2) {
-    bool expected_result = true;
-    bool actual_result = true;
-
+TEST(TestMVectorLib, try_create_object_4) {
     size_t size = 3, index = 0;
 
+    ASSERT_NO_THROW(MVector<int> object(size, { 1, 2, 3 }, index));
+}
+
+TEST(TestMVectorLib, try_create_object_with_copy) {
+    size_t size = 3, index = 0;
+    MVector<int> object1(size, { 1, 2, 3 }, index);
+
+    ASSERT_NO_THROW(MVector<int> object2(object1));
+}
+
+TEST(TestMVectorLib, correct_create_object) {
+    MVector<int> object;
+
+    EXPECT_EQ(static_cast <size_t>(0), object.start_index());
+}
+
+TEST(TestMVectorLib, correct_create_object_2) {
+    size_t start_index = 2;
+
+    MVector<int> object(10, start_index);
+
+    EXPECT_EQ(start_index, object.start_index());
+}
+
+TEST(TestMVectorLib, correct_create_object3) {
+    int size = 2;
+    int* mass = new int[size];
+    mass[0] = 1; mass[1] = 2;
+    size_t start_index = 0;
+
+    MVector<int> object(size, mass, start_index);
+    
+
+    EXPECT_EQ(start_index, object.start_index());
+}
+
+TEST(TestMVectorLib, correct_create_object_4) {
+    size_t size = 3, start_index = 1;
+
+    MVector<int> object(size, { 1, 2, 3 }, start_index);
+
+    EXPECT_EQ(start_index, object.start_index());
+}
+
+TEST(TestMVectorLib, correct_create_object_with_copy) {
+    size_t size = 3, start_index = 2;
+    MVector<int> object1(size, { 1, 2, 3 }, start_index);
+
+    MVector<int> object2(object1);
+
+    EXPECT_EQ(start_index, object2.start_index());
+}
+
+TEST(TestMVectorLib, correct_setters) {
+    size_t size = 3, old_start_index = 1, new_start_index = 2;
+    MVector<int> object(size, { 1, 2, 3 }, old_start_index);
+
+    object.set_start_index(new_start_index);
+
+    EXPECT_EQ(new_start_index, object.start_index());
+}
+
+TEST(TestMVectorLib, try_compare) {
+    bool actual_result = true;
+
+    MVector<int> object1(3, { 1, 2, 3 }, 0);
+    MVector<int> object2(object1);
+
     try {
-        MVector<int> object(size, { 1, 2, 3 }, index);
+        object1 == object2;
     }
     catch (const std::exception& ex) {
         std::cerr << ex.what();
         actual_result = false;
     }
 
-    EXPECT_EQ(expected_result, actual_result);
+    EXPECT_TRUE(actual_result);
+}
+
+TEST(TestMVectorLib, correct_compare) {
+    MVector<int> object1(3, { 1, 2, 3 }, 0);
+    MVector<int> object2(3, { 1, 2, 3 }, 0);
+
+    EXPECT_TRUE(object1 == object2);
+}
+
+TEST(TestMVectorLib, correct_compare2) {
+    MVector<int> object1(3, { 1, 2, 3 }, 0);
+    MVector<int> object2(object1);
+
+    EXPECT_TRUE(object1 == object2);
+}
+
+TEST(TestMVectorLib, try_assignment) {
+    bool actual_result = true;
+
+    MVector<int> object1(3, { 1, 2, 3 }, 0);
+    MVector<int> object2;
+
+    try {
+        object2 = object1;
+    }
+    catch (const std::exception& ex) {
+        std::cerr << ex.what();
+        actual_result = false;
+    }
+
+    EXPECT_TRUE(actual_result);
+}
+
+TEST(TestMVectorLib, correct_assignment) {
+    MVector<int> object1(3, { 1, 2, 3 }, 0);
+    MVector<int> object2;
+
+    object2 = object1;
+
+    EXPECT_TRUE(object1 == object2);
+}
+
+TEST(TestMVectorLib, try_mult) {
+    bool actual_result = true;
+
+    MVector<int> object(3, { 1, 2, 3 }, 0);
+    int value = 5;
+
+    try {
+        object * value;
+        value * object;
+    }
+    catch (const std::exception& ex) {
+        std::cerr << ex.what();
+        actual_result = false;
+    }
+
+    EXPECT_TRUE(actual_result);
+}
+
+TEST(TestMVectorLib, correct_mult) {
+    size_t size = 3, start_index = 0;
+    MVector<int> object(size, { 1, 2, 3 }, start_index);
+    int value = 3;
+
+    MVector<int> object2;
+    object2 = object * value;
+
+    MVector<int> result(size, { 3, 6, 9 }, start_index);
+
+    EXPECT_EQ(result, object2);
+}
+
+TEST(TestMVectorLib, correct_mult_2) {
+    size_t size = 3, start_index = 0;
+    MVector<int> object(size, { 1, 2, 3 }, start_index);
+    int value = 3;
+
+    MVector<int> object2;
+    object2 = value * object;
+
+    MVector<int> result(size, { 3, 6, 9 }, start_index);
+
+    EXPECT_EQ(result, object2);
+}
+
+TEST(TestMVectorLib, correct_mult_3) {
+    size_t size = 3, start_index = 0;
+    MVector<int> object1(size, { 2, 2, 2 }, start_index);
+    int value = 3;
+
+    MVector<int> object2(size, { 3, 3, 3 }, start_index);
+
+    EXPECT_EQ(18, object1 * object2);
+}
+
+TEST(TestMVectorLib, throw_uncorrect_mult) {
+    size_t start_index = 0;
+    MVector<int> object1(2, { 2, 2}, start_index);
+
+    MVector<int> object2(3, { 3, 3, 3 }, start_index);
+
+    ASSERT_THROW(object1 * object2, std::invalid_argument);
+}
+
+TEST(TestMVectorLib, throw_uncorrect_mult_2) {
+    size_t start_index = 0;
+    MVector<int> object1;
+
+    MVector<int> object2(3, { 3, 3, 3 }, start_index);
+
+    ASSERT_THROW(object1 * object2, std::invalid_argument);
+}
+
+TEST(TestMVectorLib, throw_uncorrect_mult_3) {
+    size_t start_index = 0;
+    MVector<int> object;
+    int value = 5;
+
+    ASSERT_THROW(object * value, std::invalid_argument);
+}
+
+TEST(TestMVectorLib, throw_uncorrect_mult_4) {
+    size_t start_index = 0;
+    MVector<int> object;
+    int value = 5;
+
+    ASSERT_THROW(value * object, std::invalid_argument);
+}
+
+TEST(TestMVectorLib, correct_add) {
+    size_t size = 3, start_index = 0;
+    MVector<int> object1(size, { 1, 2, 3 }, start_index);
+    MVector<int> object2(size, { 2, 3, -3 }, start_index);
+    MVector<int> result(size, { 3, 5, 0 }, start_index);
+
+    EXPECT_EQ(result, object1 + object2);
+}
+
+TEST(TestMVectorLib, correct_sub) {
+    size_t size = 3, start_index = 0;
+    MVector<int> object1(size, { 1, 2, 3 }, start_index);
+    MVector<int> object2(size, { 2, 3, -3 }, start_index);
+    MVector<int> result(size, { -1, -1, 6 }, start_index);
+
+    EXPECT_EQ(result, object1 - object2);
+}
+
+TEST(TestMVectorLib, throw_uncorrect_add) {
+    size_t start_index = 0;
+    MVector<int> object1(2, { 2, 2 }, start_index);
+
+    MVector<int> object2(3, { 3, 3, 3 }, start_index);
+
+    ASSERT_THROW(object1 + object2, std::invalid_argument);
+}
+
+TEST(TestMVectorLib, throw_uncorrect_add_2) {
+    size_t start_index = 0;
+    MVector<int> object1;
+
+    MVector<int> object2(3, { 3, 3, 3 }, start_index);
+
+    ASSERT_THROW(object1 + object2, std::invalid_argument);
+}
+
+TEST(TestMVectorLib, throw_uncorrect_sub) {
+    size_t start_index = 0;
+    MVector<int> object1(2, { 2, 2 }, start_index);
+
+    MVector<int> object2(3, { 3, 3, 3 }, start_index);
+
+    ASSERT_THROW(object1 - object2, std::invalid_argument);
+}
+
+TEST(TestMVectorLib, throw_uncorrect_sub_2) {
+    size_t start_index = 0;
+    MVector<int> object1;
+
+    MVector<int> object2(3, { 3, 3, 3 }, start_index);
+
+    ASSERT_THROW(object1 - object2, std::invalid_argument);
+}
+
+TEST(TestMVectorLib, correct_add_with_assignment) {
+    size_t size = 3, start_index = 0;
+    MVector<int> object1(size, { 1, 2, 3 }, start_index);
+    MVector<int> object2(size, { 2, 3, -3 }, start_index);
+    MVector<int> result(size, { 3, 5, 0 }, start_index);
+    object1 += object2;
+
+    EXPECT_EQ(result, object1);
+}
+
+TEST(TestMVectorLib, correct_sub_with_assignment) {
+    size_t size = 3, start_index = 0;
+    MVector<int> object1(size, { 1, 2, 3 }, start_index);
+    MVector<int> object2(size, { 2, 3, -3 }, start_index);
+    MVector<int> result(size, { -1, -1, 6 }, start_index);
+    object1 -= object2;
+
+    EXPECT_EQ(result, object1);
+}
+
+TEST(TestMVectorLib, correct_mult_with_assignment) {
+    size_t size = 3, start_index = 0;
+    MVector<int> object1(size, { 1, 2, 3 }, start_index);
+    int value = 3;
+
+    MVector<int> result(size, { 3, 6, 9 }, start_index);
+    object1 *= value;
+
+    EXPECT_EQ(result, object1);
 }
