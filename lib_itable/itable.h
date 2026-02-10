17eb6d8c5a17cc76ef5999d8300fe265548bec9a
bebra <gusol2468@gmail.com>
2026-02-10 16:53:23 +0300

    create ITable

---
+++ lib_itable/itable.h
@@ -0,0 +1,10 @@
+template <class TKey, class TValue>
+class ITable {
+public:
+	virtual ~ITable() {}
+	virtual void insert(const TKey&, const TValue&) = 0;
+	virtual void erase(const TKey&) = 0;
+	virtual TValue& found(const TKey&) const noexcept = 0;
+	virtual bool is_empty() const noexcept = 0;
+	virtual friend std::ostream& operator <<(std::ostream& out, const ITable&) = 0;
+};
