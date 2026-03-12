b3198f6ab709f5cfbff4e2697e9f5143d971d356
bebra <gusol2468@gmail.com>
2026-03-12 16:06:48 +0300

    test: add found and erase test for tree

--- tests/test_tree.cpp
+++ tests/test_tree.cpp
@@ -7,24 +7,24 @@ TEST(TestTreeLib, correct_create) {
 
 	EXPECT_TRUE(t.is_empty());
 }
-/*
+
 TEST(TestTreeLib, correct_insert_found_and_erase) {
 	Tree<int, int> t;
 	t.insert(1, 1);
 	t.insert(4, 4);
 	t.insert(1, 2);
 	t.insert(1, 3);
-	std::cout << (*t.find(1)) << " ";
+
+	EXPECT_EQ(t.find(1)->_data.value, 1);
 
 	t.insert(2, 2);
 	t.insert(2, 3);
 	t.insert(2, 4);
 
-	std::cout << (*t.find(2)) << " ";
-
-	std::cout << (*t.find(1)) << " ";
-	std::cout << (*t.find(4)) << " ";
-	std::cout << t.find(5) << "\n";
+	EXPECT_EQ(t.find(2)->_data.value, 2);
+	EXPECT_EQ(t.find(1)->_data.value, 1);
+	EXPECT_EQ(t.find(4)->_data.value, 4);
+	EXPECT_EQ(t.find(5), nullptr);
 
 	t.print_clr();
 	t.print_lcr();
@@ -34,24 +34,26 @@ TEST(TestTreeLib, correct_insert_found_and_erase) {
 	std::cout << t;
 
 	t.erase(4);
-	std::cout << t;
+	EXPECT_EQ(t.find(4), nullptr);
+	EXPECT_EQ(t.find(2)->_data.value, 4);
 
 	t.erase(1);
-	std::cout << t;
+	EXPECT_EQ(t.find(2)->_data.value, 3);
 
 	t.erase(2);
-	std::cout << t;
+	EXPECT_EQ(t.find(2)->_data.value, 2);
+	EXPECT_EQ(t.find(1)->_data.value, 2);
 
 	t.erase(2);
-	std::cout << t;
+	EXPECT_EQ(t.find(2)->_data.value, 4);
+	EXPECT_EQ(t.find(1)->_data.value, 3);
 
 	t.erase(2);
-	std::cout << t;
+	EXPECT_EQ(t.find(1)->_data.value, 3);
 
 	t.erase(1);
-	std::cout << t;
-
 	t.erase(1);
-	std::cout << t;
+
+	EXPECT_EQ(t.find(1), nullptr);
+	EXPECT_TRUE(t.is_empty());
 }
-*/
