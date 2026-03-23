c592cd3d0e563b14e5828977e41fe4bfa288e1c8
bebra <gusol2468@gmail.com>
2026-03-23 10:41:07 +0300

    fear: add heap and sort on heap in app

---
+++ lib_heap/heap.h
@@ -0,0 +1,87 @@
+#pragma once
+#include <iostream>
+//#include "../lib_tvector/tvector.h"
+#include <vector>
+
+template <class T>
+class Heap {
+    std::vector<T> _data;
+public:
+    void insert(T);
+    void pop();
+    void surfacing(int);
+    void sinking(int);
+    T get_min();
+
+    int size();
+    bool is_empty();
+};
+
+template <class T>
+void Heap<T>::insert(T elem) {
+    _data.push_back(elem);
+    surfacing(_data.size() - 1);
+}
+
+template <class T>
+void Heap<T>::pop() {
+    if (_data.empty()) {
+        throw std::logic_error("The heap is empty");
+    }
+    int size = _data.size();
+    _data[0] = _data[size - 1];
+    _data.pop_back();
+    sinking(0);
+}
+
+template <class T>
+T Heap<T>::get_min() {
+    return _data[0];
+}
+
+template <class T>
+void Heap<T>::surfacing(int ind) {
+    int parent_ind = (ind - 1) / 2;
+    while (ind > 0 && _data[parent_ind] > _data[ind]) {
+        int temp = _data[ind];
+        _data[ind] = _data[parent_ind];
+        _data[parent_ind] = temp;
+
+        ind = parent_ind;
+        parent_ind = (ind - 1) / 2;
+    }
+}
+
+template <class T>
+void Heap<T>::sinking(int ind) {
+    int left_ind, right_ind, small_ind;
+    while (true) {
+        left_ind = 2 * ind + 1;
+        right_ind = 2 * ind + 2;
+        small_ind = ind;
+
+        if (left_ind < _data.size() && _data[left_ind] < _data[small_ind]) {
+            small_ind = left_ind;
+        }
+        if (right_ind < _data.size() && _data[right_ind] < _data[small_ind]) {
+            small_ind = right_ind;
+        }
+        if (small_ind == ind) break;
+
+        int temp = _data[ind];
+        _data[ind] = _data[small_ind];
+        _data[small_ind] = temp;
+
+        ind = small_ind;
+    }
+}
+
+template <class T>
+int Heap<T>::size() {
+    return _data.size();
+}
+
+template <class T>
+bool Heap<T>::is_empty() {
+    return size() == 0;
+}
--- main/main.cpp
+++ main/main.cpp
@@ -1,18 +1,17 @@
-// Copyright 2024 Marina Usova
-
-#define EASY_EXAMPLE
-#ifdef EASY_EXAMPLE
-
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
 #include "bstree.h"
+#include "heap.h"
 
 #define SIZE 10
+//#define TREE_SORT
+#define HEAP_SORT
 
 int main() {
 	srand(static_cast<unsigned int>(time(0)));
+#ifdef TREE_SORT
 	BSTree<int, int> t;
 	int mass[SIZE];
 
@@ -29,6 +28,23 @@ int main() {
 	}
 
 	t.print_lcr();
+#endif
+#ifdef HEAP_SORT
+	Heap<int> h;
+	int mass[SIZE];
+	for (int i = 0; i < SIZE; i++) {
+		int c = rand() % 100;
+		mass[i] = c;
+	}
+	for (int i = 0; i < SIZE; i++) {
+		h.insert(mass[i]);
+	}
+
+	while (!(h.is_empty())) {
+		std::cout << h.get_min() << " ";
+		h.pop();
+	}
+#endif
 }
 
-#endif  // EASY_EXAMPLE
+
